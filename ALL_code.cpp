8478545cbcfc5c0cfeefb41f42c373e95da151d7
Kamdev Kumar <124807805+kamdev93042@users.noreply.github.com>
2025-04-15 13:47:52 +0530

    Add files via upload

---
+++ ALL_code.cpp
@@ -0,0 +1,20 @@
+1. Frequency Detectors 
+https://www.tinkercad.com/things/kcPmy5MIi40-glorious-bombul-wolt/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard%2Fdesigns%2Fcircuits&sharecode=dn8UTed-O6NZtOJA8eJo7JT-rRQAVdrE-B0eBmROHvM  
+
+2. Pressure and Temperature
+https://www.tinkercad.com/things/6HsqvPnuxBL-pressurebmp/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard%2Fdesigns%2Fcircuits&sharecode=dVCbxO05OX_gzhd3L89Lpab9pm1mRlGlgrzquveBk9c
+
+
+3. Ultrasonic sensor
+https://www.tinkercad.com/things/6ICARPh9hPS-ultrasonic-sensor/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard&sharecode=W_H820tiyujRPU8H4JXbxfypRu4pktXRngykXjQvpbk
+
+
+4. Capacitance 
+https://www.tinkercad.com/things/gjLu4lpSiKu-capacitance/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard&sharecode=Ug1G0X5l4zWlNh5x8_pjqHySY-gBf_w6kfq3Vmz6eCQ
+
+5. Resistance 
+https://www.tinkercad.com/things/62LsWz329q9-resistance/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard%2Fdesigns%2Fcircuits&sharecode=HAXsqFQFDh6P4LeWhmE_U-VOl_8gBPKas_Bo1CUt4No
+
+6. Voltmeter
+https://www.tinkercad.com/things/cnVbT1xCXTW-voltmeter/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard%2Fdesigns%2Fcircuits&sharecode=U33hYovcm9R_37ymv3oaVlfv9-ixRyHP2ImKCV3jNW0
+
